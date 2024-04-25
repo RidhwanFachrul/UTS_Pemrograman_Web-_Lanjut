e4f890e5b19e97163805ef528112885fa7d33101
RidhwanFachrul <146655228+RidhwanFachrul@users.noreply.github.com>
2024-04-25 11:06:52 +0700

    Wrong

--- uts_pemweblanjut/application/views/back/layouts/_footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white">
-   <div class="container my-auto">
-      <div class="copyright text-center my-auto">
-      <span> <?php echo date("Y"); ?> &copy; Online News Portaligniter</span>
-      </div>
-   </div>
-</footer>
